491b8a5b2e2a52c8846806dd4b657e8fd6a20f27
Ellen P. Li <Ellen.P.Li.17@dartmouth.edu>
2015-06-03 03:37:28 -0400

    added comments to startup.h

--- src/startup.h
+++ src/startup.h
@@ -1,7 +1,28 @@
+/* ========================================================================== */
+/* File: amazing_client.c
+ *
+ * Authors: Dana Silver, Shivang Sethi, Ellen Li
+ *
+ * Date: due 06/02/2015
+ *
+ * Input: N/A
+ *
+ * Output: N/A
+ *
+ * Error Conditions:
+ *
+ * Special Considerations:                             
+ */
+/* ========================================================================== */
+// ---------------- Open Issues
+
+// ---------------- System includes e.g., <stdio.h>    
+
+// ---------------- File includes
+#include "amazing_client.h"
+
 #ifndef STARTUP_H
 #define STARTUP_H
-
-#include "amazing_client.h"
 
 /*
  * integerLength - calculate the number of digits in an integer
@@ -11,8 +32,22 @@
  */
 int integerLength(int x);
 
+/*
+ * initializeLastMoves - initializes an array of last moves made
+ * by n avatars.
+ * @moveList: a pointer to the array to create
+ * @n: the number of avatars playing the game
+ *
+ * Returns 0 to indicate success and 1 to indicate failure
+ */
 int initializeLastMoves(Move **moveList, int n);
 
+
+/*
+ * printUsage - prints usage information for the program
+ *
+ * void
+ */
 void printUsage();
 
 #endif // STARTUP_H
